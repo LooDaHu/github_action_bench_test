91e40d309b91a5b1d1319df63a6e1eddce53d62c
github-action-benchmark <github@users.noreply.github.com>
2020-01-21 05:48:42 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 6210cb550319dcfacf897e6d36b570795c6ffbcf

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1579585382093,
+  "lastUpdate": 1579585722614,
   "repoUrl": "https://github.com/LooDaHu/github_action_bench_test",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -37,6 +37,43 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000009179254964524943",
             "extra": "mean: 1.0676038979591287 msec\nrounds: 931"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "651120561@qq.com",
+            "name": "LooDaHu",
+            "username": "LooDaHu"
+          },
+          "committer": {
+            "email": "PueWcyKd2xdwKBF",
+            "name": "651120561@qq.com"
+          },
+          "distinct": true,
+          "id": "6210cb550319dcfacf897e6d36b570795c6ffbcf",
+          "message": "time change",
+          "timestamp": "2020-01-20T22:48:07-07:00",
+          "tree_id": "333a8a077b95b753881a9d55563ca3f6086c4390",
+          "url": "https://github.com/LooDaHu/github_action_bench_test/commit/6210cb550319dcfacf897e6d36b570795c6ffbcf"
+        },
+        "date": 1579585722040,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "new_test.py::test_my_stuff",
+            "value": 13967.356209778609,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000018209708534719033",
+            "extra": "mean: 71.59551063070157 usec\nrounds: 11149"
+          },
+          {
+            "name": "new_test.py::test_my_stuff_different_arg",
+            "value": 884.1012789058796,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00002695412159216694",
+            "extra": "mean: 1.131092131477913 msec\nrounds: 867"
           }
         ]
       }
